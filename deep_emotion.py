a9c36585946e0a7f1df747e34e5e40b8917b9dd1
VietPT3502 <Vietdeptrai2k2@gmail.com>
2022-10-14 00:55:18 +0700

    new emotion model

--- deep_emotion.py
+++ deep_emotion.py
@@ -8,60 +8,70 @@ class Deep_Emotion(nn.Module):
         Deep_Emotion class contains the network architecture.
         '''
         super(Deep_Emotion,self).__init__()
-        self.conv1 = nn.Conv2d(1,10,3)
-        self.conv2 = nn.Conv2d(10,10,3)
-        self.pool2 = nn.MaxPool2d(2,2)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        self.batchnorm1= nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.batchnorm2 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout1 = nn.Dropout(p=0.7)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.batchnorm3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.batchnorm4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.batchnorm5 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.batchnorm6 = nn.BatchNorm2d(256)
+        self.conv7 = nn.Conv2d(in_channels=256, out_channels= 512, kernel_size=3, padding=1)
+        self.batchnorm7 = nn.BatchNorm2d(512)
+        self.conv8 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.batchnorm8 = nn.BatchNorm2d(512)
+        self.fc1 = nn.Linear(3*3*512, 128)
+        self.batchnorm9 = nn.BatchNorm1d(128)
+        self.dropout2 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(128, 512)
+        self.batchnorm10 = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, 7)
 
-        self.conv3 = nn.Conv2d(10,10,3)
-        self.conv4 = nn.Conv2d(10,10,3)
-        self.pool4 = nn.MaxPool2d(2,2)
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.batchnorm1(x)
+        x = self.relu(self.conv2(x))
+        x = self.batchnorm2(x)
+        x = self.maxpool(x)
+        x = self.dropout1(x)
 
-        self.norm = nn.BatchNorm2d(10)
+        x = self.relu(self.conv3(x))
+        x = self.batchnorm3(x)
+        x = self.relu(self.conv4(x))
+        x = self.batchnorm4(x)
+        x = self.maxpool(x)
+        x = self.dropout1(x)
 
-        self.fc1 = nn.Linear(810,50)
-        self.fc2 = nn.Linear(50,7)
+        x = self.relu(self.conv5(x))
+        x = self.batchnorm5(x)
+        x = self.relu(self.conv6(x))
+        x = self.batchnorm6(x)
+        x = self.maxpool(x)
+        x = self.dropout1(x)
 
-        self.localization = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=7),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True),
-            nn.Conv2d(8, 10, kernel_size=5),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True)
-        )
+        x = self.relu(self.conv7(x))
+        x = self.batchnorm7(x)
+        x = self.relu(self.conv8(x))
+        x = self.batchnorm8(x)
+        x = self.maxpool(x)
+        x = self.dropout1(x)
 
-        self.fc_loc = nn.Sequential(
-            nn.Linear(640, 32),
-            nn.ReLU(True),
-            nn.Linear(32, 3 * 2)
-        )
-        self.fc_loc[2].weight.data.zero_()
-        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        x = self.relu(self.batchnorm9(x))
+        x = self.dropout2(x)
 
-    def stn(self, x):
-        xs = self.localization(x)
-        xs = xs.view(-1, 640)
-        theta = self.fc_loc(xs)
-        theta = theta.view(-1, 2, 3)
+        x = self.fc2(x)
+        x = self.relu(self.batchnorm10(x))
 
-        grid = F.affine_grid(theta, x.size(), align_corners=True)
-        x = F.grid_sample(x, grid, align_corners=True)
+        x = self.fc3(x)
+
         return x
 
-    def forward(self,input):
-        out = self.stn(input)
-
-        out = F.relu(self.conv1(out))
-        out = self.conv2(out)
-        out = F.relu(self.pool2(out))
-
-        out = F.relu(self.conv3(out))
-        out = self.norm(self.conv4(out))
-        out = F.relu(self.pool4(out))
-
-        out = F.dropout(out)
-        out = out.view(-1, 810)
-        out = F.relu(self.fc1(out))
-        out = self.fc2(out)
-
-        return out
